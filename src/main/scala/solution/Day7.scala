ba0d4dba4e72a188ff2c7013883829321de94c8a
Víctor López <bigbigtor@users.noreply.github.com>
2022-12-14 22:23:28 +0100

    Day 7 part 2

--- src/main/scala/solution/Day7.scala
+++ src/main/scala/solution/Day7.scala
@@ -11,8 +11,17 @@ class Day7 {
   private val fs: FileSystem = FileSystem()
 
   private val atMostFilter: Int => Boolean = s => s <= 100000
+  
+  private val greaterThanOrEqual: Int => Boolean = s => s >= 8381165
+  
   def part1(input: String): Int =
     val rootDir = parser.parse(input)
     val dirs = fs.listAllDirectories(rootDir, atMostFilter)
     dirs.foldLeft(0)((acc, dir) => acc + dir.size)
+    
+  def part2(input: String): Int =
+    val rootDir = parser.parse(input)
+    fs.listAllDirectories(rootDir, greaterThanOrEqual)
+      .map(_.size)
+      .min
 }
